a0b1d652bc56219e85a53e0ebdaab04bceac969f
esra.evren <eevren412@gmail.com>
2025-03-30 21:20:42 +0300

    Proje dosyası ekle.

---
+++ RabbitMq.Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Threading.Tasks;
+using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://zildxkuy:ZRZBuEUMuNRG7NM2eu8qU_JKyioBK8-X@jackal.rmq.cloudamqp.com/zildxkuy");
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        channel.QueueDeclare("hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+        string message = "Hello World!";
+        var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "", routingKey: "hello-queue", basicProperties: null, body: body);
+
+        Console.WriteLine("Message sent!");
+        Console.ReadLine();
+    }
+}
---
+++ RabbitMq.Subscriber/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory();
+        factory.Uri = new Uri("amqps://zildxkuy:ZRZBuEUMuNRG7NM2eu8qU_JKyioBK8-X@jackal.rmq.cloudamqp.com/zildxkuy");
+
+        using var connection = factory.CreateConnection();
+        using var channel = connection.CreateModel();
+        // channel.QueueDeclare("hello-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        channel.BasicConsume("hello-queue", autoAck: true, consumer);
+
+        consumer.Received += (sender, eventArgs) =>
+        {
+            var body = eventArgs.Body.ToArray();
+            var message = System.Text.Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Message received: {message}");
+
+        };
+
+    }
+}
